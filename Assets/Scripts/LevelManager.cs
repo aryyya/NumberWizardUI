1e64489be382c9f77e05ce538e1a0d1a7488caa7
aryyya <arya@live.ca>
2018-06-12 00:05:59 -0700

    Add scenes to build settings.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public void LoadLevel(string name)
     {
-        Debug.Log("Loading level: " + name + ".");
+        SceneManager.LoadScene(name);
     }
 
     public void QuitGame()
     {
-        Debug.Log("Quitting game.");
+        Application.Quit();
     }
 }
